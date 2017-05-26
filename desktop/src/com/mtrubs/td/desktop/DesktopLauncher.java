08dc0daf722aa05a0ae73f31343e2bf87071f2a2
mrubino15 <Matthew.Rubino@teamaol.com>
2017-05-26 11:23:55 -0400

    project creation

---
+++ desktop/src/com/mtrubs/td/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mtrubs.td.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mtrubs.td.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
