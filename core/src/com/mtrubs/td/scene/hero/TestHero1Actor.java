0fd2ba09463ce26f4900e1e9ad996339f6c9ea84
mrubino15 <Matthew.Rubino@teamaol.com>
2017-06-17 14:03:50 -0400

    add a drag to move heroes

--- core/src/com/mtrubs/td/scene/hero/TestHero1Actor.java
+++ core/src/com/mtrubs/td/scene/hero/TestHero1Actor.java
@@ -1,14 +1,64 @@
 package com.mtrubs.td.scene.hero;
 
+import aurelienribon.tweenengine.Timeline;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenEquations;
+import aurelienribon.tweenengine.TweenManager;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mtrubs.td.graphics.HeroUnit;
 import com.mtrubs.td.graphics.ProjectileType;
 import com.mtrubs.td.graphics.TextureRegionManager;
 import com.mtrubs.td.scene.CombatActor;
+import com.mtrubs.td.scene.LevelStage;
+import com.mtrubs.td.scene.TextureRegionActorAccessor;
 
 public class TestHero1Actor extends CombatActor {
 
+  private Timeline timeline;
+  private float speed = 25.0F;
+
   public TestHero1Actor(TextureRegionManager textureRegionManager, float startX, float startY) {
     super(startX, startY, textureRegionManager.get(HeroUnit.TestHero1));
+
+    addListener(new ClickListener() {
+
+      @Override
+      public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+        super.touchDown(event, x, y, pointer, button);
+        return true; // to intercept for the touch up event
+      }
+
+      @Override
+      public void touchUp(InputEvent event, float xDelta, float yDelta, int pointer, int button) {
+        super.touchUp(event, xDelta, yDelta, pointer, button);
+        moveTo(getCenter(), event.getStageX(), event.getStageY());
+      }
+    });
+  }
+
+  private void moveTo(Vector2 from, float toX, float toY) {
+    // adjust to be the center
+    Vector2 to = new Vector2(toX - getWidth() / 2.0F, toY - getHeight() / 2.0F);
+
+    // stop other timelines associated with this
+    getTweenManager().killTarget(this);
+    // move to the position
+    this.timeline = Timeline.createSequence().delay(0.0F);
+    float duration = getDuration(from, to);
+    this.timeline.push(Tween.to(this,
+      TextureRegionActorAccessor.POSITION_XY, duration).target(to.x, to.y).ease(TweenEquations.easeNone));
+    this.timeline.start(getTweenManager());
+  }
+
+  // TODO: this logic (and thus for MobActors too) doesnt seem quite right
+  private float getDuration(Vector2 a, Vector2 b) {
+    return a.dst(b) / this.speed;
+  }
+
+  private TweenManager getTweenManager() {
+    return ((LevelStage) getStage()).getTweenManager();
   }
 
   @Override
